a07cc4f15b7f0e6df5293f31b59791d8fc489790
stormy131 <artemkopyl1990@gmail.com>
2022-02-10 15:41:47 +0200

    found bug in algorithm

--- algorithm.js
+++ algorithm.js
@@ -1,8 +1,7 @@
 const solve = (a, b, c) => {
     const D = b ** 2 - 4 * a * c;
     const x1 = (-b + Math.sqrt(D)) / (2 * a);
-    const x2 = (-b + Math.sqrt(D)) / (2 * a);
-
+    const x2 = (-b - Math.sqrt(D)) / (2 * a);
     return new Set(D >= 0 ? [x1, x2] : null);
 };
 
